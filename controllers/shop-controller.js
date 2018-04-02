90c6435da47b917482c78fe1ca9d85142cbe2d27
nanigalli <ngalli@navent.com>
2018-04-01 21:13:52 -0300

    Agregue el metodo para el endpoint de crear un Shop

--- controllers/shop-controller.js
+++ controllers/shop-controller.js
@@ -1,7 +1,23 @@
+const Shop = require('../model/Shop');
+const responseUtils = require('../utils/response-utils');
+
 exports.getShops = function (req, res) {
     // TODO : LLAMAR AL MODELO DE SHOPS
     const shops = [
         { id:1 , name: 'mercadito', address: 'Sto domingo 1180', zone: 'Martinez', phone: '123', enabled: true }
     ];
+    
     res.send({ shops });
+};
+
+exports.createShop = function(req , res) {
+    const shopJson = req.body;
+    Shop.insert(shopJson)
+        .then( ([shop]) => {
+            res.send({ shop });
+        })
+        .catch(cause => {
+            console.error("Hubo un problema al crear el Shop: " + JSON.stringify(shopJson));
+            responseUtils.sendMsgCodeResponse(res , "Hubo un problema al crear el Shop: " + JSON.stringify(shopJson) , 500);
+        })
 };
