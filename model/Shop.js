d7839be63ebcda5ab1dbf0a84ab35a12d47b7ac1
nanigalli <ngalli@navent.com>
2018-04-02 15:24:11 -0300

    Agregue el campo enabled a los negocios, va a servir para identificar si un shop esta habilitado o no

--- model/Shop.js
+++ model/Shop.js
@@ -3,12 +3,13 @@ const dbManager = require('./db-manager');
 const table = 'shops';
 const idType = 'SERIAL'
 
-function Shop(id , name, address, zone, phone) {
+function Shop(id, name, address, zone, phone, enabled) {
     this.id = id;
     this.name = name;
     this.address = address;
     this.phone = phone
     this.zone = zone;
+    this.enabled = enabled;
 }
 
 Shop.createTable = function () {
@@ -17,26 +18,34 @@ Shop.createTable = function () {
         name VARCHAR(64),
         address VARCHAR(64),
         phone VARCHAR(32),
-        zone VARCHAR(64)
+        zone VARCHAR(64),
+        enabled BOOLEAN DEFAULT TRUE
     )`;
-    return dbManager.queryPromise(sql,[]);
+    return dbManager.queryPromise(sql, []);
 }
 
-Shop.insert = function(json) {
-    const {name, address, phone, zone} = json;
+Shop.insert = function (json) {
+    const { name, address, phone, zone } = json;
     const sql = `INSERT INTO ${table}(name, address, phone, zone) VALUES($1,$2,$3,$4) RETURNING *`;
     const values = [name, address, phone, zone];
     return dbManager.queryPromise(sql, values, fromRows);
 }
 
 Shop.update = function (json) {
-    const {id, name, address, phone, zone} = json;
+    const { id, name, address, phone, zone } = json;
     const sql = `UPDATE ${table} SET name=$1, address=$2, phone=$3, zone=$4 WHERE id=$5 RETURNING *`;
     const values = [name, address, phone, zone, id];
     return dbManager.queryPromise(sql, values, fromRows);
 };
 
-Shop.deleteById = function(shop) {
+Shop.setEnabled = function (json) {
+    const { id, enabled } = json;
+    const sql = `UPDATE ${table} SET enabled=$1 WHERE id=$2 RETURNING *`;
+    const values = [enabled, id];
+    return dbManager.queryPromise(sql, values, fromRows);
+};
+
+Shop.deleteById = function (shop) {
     const id = shop.id || shop;
     const sql = `DELETE FROM ${table} WHERE id=$1 RETURNING *`;
     return dbManager.queryPromise(sql, [id], fromRows);
@@ -47,31 +56,32 @@ Shop.deleteById = function(shop) {
  * @param {object} json Objeto a partir del cual crear el shop 
  */
 function fromJson(json) {
-    const {id , name, address, phone, zone} = json;
-    return new Shop(id, name, address, phone, zone);
+    const { id, name, address, phone, zone, enabled = true } = json;
+    return new Shop(id, name, address, phone, zone, enabled);
 }
 
 function fromRows(rows) {
-    return rows.map( fromJson );
+    return rows.map(fromJson);
 }
 
-Shop.findById = function(shop) {
+Shop.findById = function (shop) {
     /* esta expresion determina que si shop tiene un atributo id (xq es un objeto) 
     entonces asigna a la variable id el valor de dicho atributo.
     Caso contrario, interpreto a shop como un numero y lo asigno a id.
     nota: esto vale si y solo si el operando de la izquierda no es "falsy" por efecto colateral */
     const id = shop.id || shop;
     const sql = `SELECT * FROM ${table} WHERE id=$1`;
-    return dbManager.queryPromise(sql , [id] , fromRows);
+    return dbManager.queryPromise(sql, [id], fromRows);
 };
 
-Shop.findEverything = function() {
-    /* esta expresion determina que si shop tiene un atributo id (xq es un objeto) 
-    entonces asigna a la variable id el valor de dicho atributo.
-    Caso contrario, interpreto a shop como un numero y lo asigno a id.
-    nota: esto vale si y solo si el operando de la izquierda no es "falsy" por efecto colateral */
+Shop.find = function () {
     const sql = `SELECT * FROM ${table}`;
-    return dbManager.queryPromise(sql , [] , fromRows);
+    return dbManager.queryPromise(sql, [], fromRows);
+};
+
+Shop.findEnabled = function () {
+    const sql = `SELECT * FROM ${table} WHERE enabled = TRUE`;
+    return dbManager.queryPromise(sql, [], fromRows);
 };
 
 // EJEMPLO de llamada a findById
