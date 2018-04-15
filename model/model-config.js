23466a5b0b16c1cf39840cfd1532c59f967a71a6
Nadia Galli <nani16.galli@gmail.com>
2018-04-15 15:54:48 -0300

    Vuelvo atras un cambio de el host de la base de datos que se colo en otro commit

--- model/model-config.js
+++ model/model-config.js
@@ -3,7 +3,7 @@ module.exports = {
     password : 'root',
     /* La url del host de la BBDD es la de DOCKER.
     Si POSTGRES es instalado en el equipo local, se puede usar localhost */
-    host : 'localhost',
+    host : '192.168.99.100',
     db : 'hoycomo',
     port : '5432'
 };
